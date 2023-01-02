c9266703e806f9aa13fb14c35db6077eba078d7a
luccie-cmd <115068339+luccie-cmd@users.noreply.github.com>
2023-01-02 09:57:21 +0100

    Update main.cpp

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,8 +1,10 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <iostream>
-
 #include "RenderWindow.hpp"
+
+#define _window_width 1280
+#define _window_height 720
 
 
 void Init(){
@@ -14,18 +16,12 @@ void Init(){
     }
 }
 
-int window_width = 1280;
-int window_height = 720;
 bool IsRunning = true;
 SDL_Event events;
-
-#define _window_width window_width
-#define _window_height window_height
 
 int WinMain(int argc, char* argv){
     Init();
     RenderWindow window("Tile", _window_width, _window_height);
-    SDL_Texture* bg_Nova = window.LoadTexture("S:/Code/res/gfx/Background_Nova_Lynn.png");
     SDL_Texture* bg = window.LoadTexture("S:/Code/res/gfx/Background.png");
     SDL_Texture* grass = window.LoadTexture("S:/Code/res/gfx/Grass.png");
     int* plat = NULL;
